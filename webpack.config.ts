82c6d5dc920422e9f3370ac48fc37445f523e391
Александр Зайцев <60178690+katsuAZ@users.noreply.github.com>
2025-04-11 14:00:42 +0300

    Add files via upload

---
+++ webpack.config.ts
@@ -0,0 +1,62 @@
+import path from 'path';
+import webpack from 'webpack';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+import type { Configuration as DevServerConfiguration } from 'webpack-dev-server';
+
+type Mode = 'production' | 'development';
+
+interface EnvVariables {
+    mode: Mode;
+    port: number;
+}
+
+export default (env: EnvVariables) => {
+    const isDev = env.mode === 'development';
+
+    const config: webpack.Configuration = {
+        mode: env.mode ?? 'development',
+        entry: path.resolve(__dirname, 'src', 'index.tsx'),
+        output: {
+            path: path.resolve(__dirname, 'build'),
+            filename: '[name].[contenthash].js',
+            clean: true,
+        },
+        plugins: [
+            new HtmlWebpackPlugin({
+                template: path.resolve(__dirname, 'public', 'index.html'),
+            }),
+            isDev && new webpack.ProgressPlugin(),
+        ].filter(Boolean),
+        module: {
+            rules: [
+                {
+                    test: /\.s[ac]ss$/i,
+                    use: [
+                        // Creates `style` nodes from JS strings
+                        'style-loader',
+                        // Translates CSS into CommonJS
+                        'css-loader',
+                        // Compiles Sass to CSS
+                        'sass-loader',
+                    ],
+                },
+                {
+                    test: /\.tsx?$/,
+                    use: 'ts-loader',
+                    exclude: /node_modules/,
+                },
+            ],
+        },
+        resolve: {
+            extensions: ['.tsx', '.ts', '.js'],
+        },
+        devtool: isDev && 'inline-source-map',
+        devServer: isDev
+            ? {
+                  port: env.port ?? 3000,
+                  open: true,
+              }
+            : undefined,
+    };
+    return config;
+};
