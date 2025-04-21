14b334afd4c8383fc6c63f13ec4f4d590a642912
katsu <pibpivo@gmail.com>
2025-04-22 00:20:59 +0300

    upd babel

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -2,7 +2,7 @@ import {ModuleOptions} from 'webpack';
 import MiniCssExtractPlugin from 'mini-css-extract-plugin';
 import {BuildOptions} from './types/types';
 import ReactRefreshTypeScript from 'react-refresh-typescript';
-import {before} from 'node:test';
+import {buildBabelLoader} from "./babel/buildBabelLoader";
 
 export function buildLoaders(options: BuildOptions): ModuleOptions['rules'] {
     const isDev = options.mode === 'development';
@@ -73,22 +73,7 @@ export function buildLoaders(options: BuildOptions): ModuleOptions['rules'] {
         ],
     };
 
-    const babelLoader = {
-        test: /\.tsx?$/,
-        exclude: /node_modules/,
-        use: {
-            loader: "babel-loader",
-            options: {
-                presets: [
-                    '@babel/preset-env',
-                    "@babel/preset-typescript",
-                    ["@babel/preset-react", {
-                        runtime: isDev ? 'automatic' : 'classic',
-                    }]
-                ]
-            }
-        }
-    }
+    const babelLoader = buildBabelLoader(options);
 
     return [assetLoader,
         scssLoader,
