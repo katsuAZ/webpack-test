be2413d1c79a91f6c78d6acf5084a86491704270
katsu <pibpivo@gmail.com>
2025-04-21 23:10:30 +0300

    add babel loader

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -1,8 +1,8 @@
-import { ModuleOptions } from 'webpack';
+import {ModuleOptions} from 'webpack';
 import MiniCssExtractPlugin from 'mini-css-extract-plugin';
-import { BuildOptions } from './types/types';
+import {BuildOptions} from './types/types';
 import ReactRefreshTypeScript from 'react-refresh-typescript';
-import { before } from 'node:test';
+import {before} from 'node:test';
 
 export function buildLoaders(options: BuildOptions): ModuleOptions['rules'] {
     const isDev = options.mode === 'development';
@@ -59,21 +59,40 @@ export function buildLoaders(options: BuildOptions): ModuleOptions['rules'] {
 
     const tsLoader = {
         test: /\.tsx?$/,
+        exclude: /node_modules/,
         use: [
             {
                 loader: 'ts-loader',
                 options: {
                     transpileOnly: true,
                     getCustomTransformers: () => ({
-                        before: [isDev && ReactRefreshTypeScript()].filter(
-                            Boolean
-                        ),
+                        before: [isDev && ReactRefreshTypeScript()].filter(Boolean),
                     }),
                 },
             },
         ],
-        exclude: /node_modules/,
     };
 
-    return [assetLoader, scssLoader, tsLoader, svgrLoader];
+    const babelLoader = {
+        test: /\.tsx?$/,
+        exclude: /node_modules/,
+        use: {
+            loader: "babel-loader",
+            options: {
+                presets: [
+                    '@babel/preset-env',
+                    "@babel/preset-typescript",
+                    ["@babel/preset-react", {
+                        runtime: isDev ? 'automatic' : 'classic',
+                    }]
+                ]
+            }
+        }
+    }
+
+    return [assetLoader,
+        scssLoader,
+        // tsLoader
+        babelLoader,
+        svgrLoader];
 }
